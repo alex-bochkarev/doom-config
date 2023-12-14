6469805338985d0557ff7fe0fb9b30f8ab64a1a8
Alexey Bochkarev <a@bochkarev.io>
2023-12-14 12:24:24 +0100

    bugfix caldav + rss, irc

--- calendars.el
+++ calendars.el
@@ -11,7 +11,11 @@
 (setq org-caldav-files '("~/PKB/notes/20231205101723-bo_vqe_milena_roers_bsc.org"
                          "~/PKB/notes/proj-notes/QuanTUK/project.org"
                          "~/PKB/notes/20231026102330-grover_for_sat_project.org"
-                         "~/PKB/notes/conferences.org"))
+                         "~/PKB/notes/conferences.org"
+                         "~/PKB/notes/proj-notes/2024_BSc_Daniel_Theis/project.org"))
 
 (setq org-icalendar-timezone "Europe/Berlin")
 (setq org-caldav-select-tags '("cal"))
+
+(setq org-icalendar-use-scheduled '(todo-start event-if-not-todo))
+(setq org-icalendar-use-deadline '(event-if-not-todo todo-due))
